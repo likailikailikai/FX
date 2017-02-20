0d8ae55a244b1b78e57956fff63c35f75d713bb8
情v枫 <1360869846@qq.com>
2017-02-20 16:08:36 +0800

    群监听的实现

--- app/src/main/java/com/atguigu/fx/utils/Contacts.java
+++ app/src/main/java/com/atguigu/fx/utils/Contacts.java
@@ -1,7 +1,6 @@
 package com.atguigu.fx.utils;
 
 
-
 /**
  * Created by 情v枫 on 2017/2/16.
  */
@@ -12,4 +11,7 @@ public class Contacts {
     public static final String NEW_INVITE_CHANGE = "new_invite_change";
     //联系人状态
     public static final String CONTACT_CHANGE = "contact_change";
+
+    //群邀请状态
+    public static final String GROUP_INVITE_CHAGE = "group_invite_chage";
 }
